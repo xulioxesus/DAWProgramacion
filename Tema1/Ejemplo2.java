3f97265738e1e6857a1b726eaf5746156c903047
Julio Jesus Leon Perez <xulioxesus@gmail.com>
2021-09-29 10:06:50 +0200

    Arreglado E2 y hecho E3

--- Tema1/Ejemplo2.java
+++ Tema1/Ejemplo2.java
@@ -1,12 +1,12 @@
-class Ejemplo1
+class Ejemplo2
 {
     public static void main(String args[])
     {
-    	int dividendo = 20.0;
+    	int dividendo = (int) 20.0;
     	
-    	int divisor = 6.0;
+    	int divisor = (int) 6.0;
     	
-    	int sumarAlFinal = 3.0;
+    	int sumarAlFinal = (int) 3.0;
     	
         System.out.println((dividendo / divisor) + sumarAlFinal);
     }
