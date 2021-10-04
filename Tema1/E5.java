25fb7ba580ee341996fb2a8efbc8cf2f2c0f1037
Julio Jesus Leon Perez <xulioxesus@gmail.com>
2021-10-04 09:00:04 +0200

    Finalizado E5

---
+++ Tema1/E5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class E5 {
+    public static void main(String[] args) {
+        
+        // Declara tus variables
+        Scanner lector = new Scanner(System.in);
+        int x, y;
+        int sum, sub, mult, div;
+
+        // Inicio del programa
+
+        System.out.println("Enter number 1:");
+        x = lector.nextInt();
+
+        System.out.println("Enter number 2:");
+        y = lector.nextInt();
+
+        sum = x + y;
+        sub = x - y;
+        mult = x * y;
+        div = x / y;
+
+        System.out.println("sum: " + sum);
+        System.out.println("sub: " + sub);
+        System.out.println("mult: " + mult);
+        System.out.println("div: " + sum);
+
+    }
+}
