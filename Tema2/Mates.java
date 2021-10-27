9b9ff5c0f22b5207c7ab03cec36a3fa3bc632278
Julio Jesus Leon Perez <xulioxesus@gmail.com>
2021-10-27 10:34:37 +0200

    Acabado MatesPrueba

--- Tema2/Mates.java
+++ Tema2/Mates.java
@@ -16,10 +16,7 @@ public class Mates {
         return x / y;
     }
 
-    public static void main(String[] args) {
-        System.out.println("suma: 25 es igual a " + suma(12,13));
-        System.out.println("resta: -1 es igual a " + resta(12,13));
-        System.out.println("multiplicacion: 156 es igual a " + multiplicacion(12,13));
-        System.out.println("division: 0.923 es igual a " + division(12,13));
+    public static double areaCirculo(double radio){
+        return Math.PI * radio * radio;
     }
 }
