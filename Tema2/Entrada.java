6a2659ff6d2802b97d8302aa67e5220f8f67d7e9
xulioxesus <xulioxesus@gmail.com>
2021-11-02 18:53:08 +0100

    Finalizado ejercicio de Entrada

--- Tema2/Entrada.java
+++ Tema2/Entrada.java
@@ -5,25 +5,22 @@ public class Entrada {
         int resultado = 0;
         String mensaje = "Escriba un entero: ";
         Scanner lector = new Scanner(System.in);
-        boolean hayEntero = false;
+        boolean enteroLeido = false;
 
         System.out.println(mensaje);
-        while (!hayEntero){
-            if(lector.hasNextInt()){
+        while (!enteroLeido){
+            if(lector.hasNextLine() && lector.hasNextInt()){
                 resultado = lector.nextInt();
-                hayEntero = true;
-                lector.nextLine();
+                
+                enteroLeido = true;
             }else{
-                lector.nextLine();
                 System.out.println(mensaje);
+                
             }
-        }
-
-        if (lector.hasNextLine()){
             lector.nextLine();
         }
 
-        lector.close();
+        //lector.close();
 
         return resultado;
     }
