7aceacda1213a317f7c96087184ee688f6a88815
xulioxesus <xulioxesus@gmail.com>
2021-11-04 21:31:44 +0100

    Commit inicial Acepta el reto

---
+++ AceptaElReto/P116HolaMundo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P116HolaMundo {
+
+    public static void main(String[] args) {
+
+        Scanner lector = new Scanner(System.in);
+        int cantidad;
+        
+        cantidad = lector.nextInt();
+
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Hola mundo.");
+        }
+    }
+}
