b67226bfc6849ef418384a77fa5dbfb0cd55ce4f
Julio Jesus Leon Perez <xulioxesus@gmail.com>
2021-12-17 08:43:28 +0100

    Ejercicio calculadora

---
+++ Tema3/Calculadora.md
@@ -0,0 +1,17 @@
+# Calculadora
+
+Crea un programa llamado Calculadora.
+
+El programa debe ir leyendo las operaciones que introduce el usuario:
+
+- 3       + 6
+- 3456.45    x    89
+- asdlkfjlsdkf 4 + 7
+
+Debe devolver el resultado o el mensaje de "Operación incorrecta"
+
+El programa debe terminar si se escribe "fin" o "Fin" o "FIN". Es decir, la palabra fin sin tener en cuenta minúsculas ni mayúsculas.
+
+Las operaciones válidas son:
+
++, - , / y (x o X) 
--- Tema3/Victor/Comparar.java
+++ Tema3/Victor/Comparar.java
@@ -5,14 +5,44 @@ package strings;
  */
 public class Comparar {
 	public static int comparar(String cadena1, String cadena2) {
-		return cadena1.compareTo(cadena2);
+		return  cadena1.compareTo(cadena2);
 	}
 	public static void main(String[] args) {
 		
-		String cadena1 = Utilidades.leerCadena("Introduce cadena1");
-		String cadena2 = Utilidades.leerCadena("Introduce cadena2");
-		int resultado = comparar(cadena1, cadena2);
+		String cadena1 = "Hola pepito";
+		String cadena2 = "Au cacau";
+		int resultado = 0;
+		resultado = comparar(cadena1,cadena2);
+		imprimirResultadoComparacion(resultado, cadena1, cadena2);
+
+		cadena1 = "";
+		cadena2 = "Au cacau";
+		resultado = comparar(cadena1,cadena2);
+		imprimirResultadoComparacion(resultado, cadena1, cadena2);
+
+		cadena1 = "Hola pepito";
+		cadena2 = "Hola pepito";
+		resultado = comparar(cadena1,cadena2);
+		imprimirResultadoComparacion(resultado, cadena1, cadena2);
+
+		cadena1 = "Hola pepito";
+		cadena2 = "1234";
+		resultado = comparar(cadena1,cadena2);
+		imprimirResultadoComparacion(resultado, cadena1, cadena2);
 		
+		cadena1 = "aaa";
+		cadena2 = "AAA";
+		resultado = comparar(cadena1,cadena2);
+		imprimirResultadoComparacion(resultado, cadena1, cadena2);
+
+		char letra = '9';
+		letra += 3;
+		
+		System.out.println(letra);
+		
+	}
+	
+	public static void imprimirResultadoComparacion(int resultado, String cadena1, String cadena2) {
 		if (0 == resultado){
 			System.out.println("Las cadenas son iguales");
 		}
